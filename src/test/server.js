58a8f341389c2493f302da5b9c4322fe608714b9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-11-17 22:07:55 -0500

    added simple sanity test

---
+++ src/test/server.js
@@ -0,0 +1,33 @@
+var listen = require('../app');
+var request = require('request');
+
+var port = 4444;
+var urlBase = 'http://localhost:' + port + '/';
+
+gt.module('server', {
+  setupOnce: function () {
+    console.log('starting listening');
+    listen(port);
+  }
+});
+
+gt.async('no username', function () {
+  var url = urlBase;
+  request(url, function (err, response, body) {
+    if (err) throw err;
+    gt.equal(response.statusCode, 401, '401 without username');
+    gt.start();
+  });
+});
+
+gt.async('jashkenas', function () {
+  var url = urlBase + 'jashkenas';
+  request(url, function (err, response, body) {
+    if (err) throw err;
+    gt.equal(response.statusCode, 200, 'got response');
+    gt.ok(/jashkenas/.test(body), 'contains username');
+    gt.ok(/Total/i.test(body), 'contains total');
+    gt.start();
+  });
+});
+
